7d3e97b095c9fb676f3e7013b970443df19d8713
Samuel Georges <sam@daftspunk.com>
2026-02-28 16:19:37 +1100

    Formatting

--- assets/js/controls/checkout-form.js
+++ assets/js/controls/checkout-form.js
@@ -1,79 +1,75 @@
-;(function(){
+oc.registerControl('checkout-form', class extends oc.ControlBase {
+    connect() {
+        this.listen('change', '[name=city],[name=zip],[name=state_id],[name=country_id]', this.onChangeContactDetails);
+        this.listen('change', '[name=shipping_method]', this.onChangeShippingMethod);
+        this.listen('change', '[name=payment_method]', this.onChangePaymentMethod);
+        this.listen('change', '[name=address_book_id]', this.onChangeAddressBook);
+        addEventListener('pay:fetch-invoice', this.proxy(this.onFetchInvoiceHash));
+    }
 
-    oc.registerControl('checkout-form', class extends oc.ControlBase {
-        connect() {
-            this.listen('change', '[name=city],[name=zip],[name=state_id],[name=country_id]', this.onChangeContactDetails);
-            this.listen('change', '[name=shipping_method]', this.onChangeShippingMethod);
-            this.listen('change', '[name=payment_method]', this.onChangePaymentMethod);
-            this.listen('change', '[name=address_book_id]', this.onChangeAddressBook);
-            addEventListener('pay:fetch-invoice', this.proxy(this.onFetchInvoiceHash));
-        }
+    disconnect() {
+        removeEventListener('pay:fetch-invoice', this.proxy(this.onFetchInvoiceHash));
+    }
 
-        disconnect() {
-            removeEventListener('pay:fetch-invoice', this.proxy(this.onFetchInvoiceHash));
-        }
+    onFetchInvoiceHash(event) {
+        event.detail.fetchFunc = async () => {
+            const data = await oc.request(this.element, 'onPrepareOrder', { async: true });
+            return data.invoice_hash;
+        };
+    }
 
-        onFetchInvoiceHash(event) {
-            event.detail.fetchFunc = async () => {
-                const data = await oc.request(this.element, 'onPrepareOrder', { async: true });
-                return data.invoice_hash;
-            };
-        }
+    onChangeAddressBook() {
+        oc.request(this.element, 'onAction', {
+            data: {
+                post_address_book_preset: true,
+                skip_validation: true
+            },
+            update: {
+                'shop/checkout-step-details': '#shopCheckoutStepDetails',
+                'shop/checkout-step-shipping': '#shopCheckoutStepShipping',
+                'shop/checkout-step-payment': '#shopCheckoutStepPayment',
+                'shop/order-summary': '#shopCheckoutOrderSummary'
+            }
+        });
+    }
 
-        onChangeAddressBook() {
-            oc.request(this.element, 'onAction', {
-                data: {
-                    post_address_book_preset: true,
-                    skip_validation: true
-                },
-                update: {
-                    'shop/checkout-step-details': '#shopCheckoutStepDetails',
-                    'shop/checkout-step-shipping': '#shopCheckoutStepShipping',
-                    'shop/checkout-step-payment': '#shopCheckoutStepPayment',
-                    'shop/order-summary': '#shopCheckoutOrderSummary'
-                }
-            });
-        }
+    onChangeContactDetails() {
+        oc.request(this.element, 'onAction', {
+            data: {
+                post_contact_details: true,
+                skip_validation: true
+            },
+            update: {
+                'shop/checkout-step-shipping': '#shopCheckoutStepShipping',
+                'shop/checkout-step-payment': '#shopCheckoutStepPayment',
+                'shop/order-summary': '#shopCheckoutOrderSummary'
+            }
+        });
+    }
 
-        onChangeContactDetails() {
-            oc.request(this.element, 'onAction', {
-                data: {
-                    post_contact_details: true,
-                    skip_validation: true
-                },
-                update: {
-                    'shop/checkout-step-shipping': '#shopCheckoutStepShipping',
-                    'shop/checkout-step-payment': '#shopCheckoutStepPayment',
-                    'shop/order-summary': '#shopCheckoutOrderSummary'
-                }
-            });
-        }
+    onChangeShippingMethod() {
+        oc.request(this.element, 'onAction', {
+            data: {
+                post_shipping_method: true,
+                skip_validation: true
+            },
+            update: {
+                'shop/checkout-step-payment': '#shopCheckoutStepPayment',
+                'shop/order-summary': '#shopCheckoutOrderSummary'
+            }
+        });
+    }
 
-        onChangeShippingMethod() {
-            oc.request(this.element, 'onAction', {
-                data: {
-                    post_shipping_method: true,
-                    skip_validation: true
-                },
-                update: {
-                    'shop/checkout-step-payment': '#shopCheckoutStepPayment',
-                    'shop/order-summary': '#shopCheckoutOrderSummary'
-                }
-            });
-        }
-
-        onChangePaymentMethod() {
-            oc.request(this.element, 'onAction', {
-                data: {
-                    post_payment_method: true,
-                    skip_validation: true
-                },
-                update: {
-                    'shop/payment-form': '#shopPaymentForm',
-                    'shop/order-summary': '#shopCheckoutOrderSummary'
-                }
-            });
-        }
-    });
-
-})();
+    onChangePaymentMethod() {
+        oc.request(this.element, 'onAction', {
+            data: {
+                post_payment_method: true,
+                skip_validation: true
+            },
+            update: {
+                'shop/payment-form': '#shopPaymentForm',
+                'shop/order-summary': '#shopCheckoutOrderSummary'
+            }
+        });
+    }
+});
