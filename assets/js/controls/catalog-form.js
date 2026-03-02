7b1f79bb9ab91b137dc4042b8a7669cb971f8f27
Samuel Georges <sam@daftspunk.com>
2026-03-02 19:12:52 +1100

    Filter by rating

--- assets/js/controls/catalog-form.js
+++ assets/js/controls/catalog-form.js
@@ -1,16 +1,30 @@
 class CatalogForm extends oc.ControlBase {
     connect() {
         this.listen('change', '[data-filter-manufacturer]', this.onFilterProducts);
+        this.listen('change', '[data-filter-rating]', this.onFilterProducts);
     }
 
     onFilterProducts() {
+        const data = {};
+
         const manufacturers = [];
         this.element.querySelectorAll('[data-filter-manufacturer]:checked').forEach(function(el) {
             manufacturers.push(el.value);
         });
+        if (manufacturers.length) {
+            data.manufacturers = manufacturers;
+        }
+
+        const ratings = [];
+        this.element.querySelectorAll('[data-filter-rating]:checked').forEach(function(el) {
+            ratings.push(el.value);
+        });
+        if (ratings.length) {
+            data.ratings = ratings;
+        }
 
         oc.request(this.element, 'onAction', {
-            data: { manufacturers: manufacturers },
+            data: data,
             update: {
                 'shop-category/category-products': '#categoryProducts'
             }
